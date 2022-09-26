96b740e8d938c3bb68746b5148030c2beb4333f0
balgutria <balgutria@motus.com>
2022-09-26 10:35:35 -0500

    Testing cron

--- index.ts
+++ index.ts
@@ -2,38 +2,41 @@ import { chromium } from 'playwright'
 import notifier from 'node-notifier'
 import cron from 'node-cron'
 
-cron.schedule("20 0 17-17 * * *", async () => {
+// cron.schedule("20 0 17 * * *", async () => {
+cron.schedule("* * * * * *", async () => {
   console.log(`Running on: ${new Date().toLocaleString('es-CO', { timeZone: 'America/Bogota' })}`)
+  console.log("Hey!");
+  
 
-  const browser = await chromium.launch()
-  const page = await browser.newPage()
-  await page.goto('https://appbaq.barranquilla.gov.co:8989/canchas/agendamiento')
+  // const browser = await chromium.launch()
+  // const page = await browser.newPage()
+  // await page.goto('https://appbaq.barranquilla.gov.co:8989/canchas/agendamiento')
 
-  await page.click('#aviso .close');
-  await page.selectOption('#sedes_id', {label: 'Parque Bosques del Norte'});
-  await page.selectOption('#deportes_id', {label: 'Tenis'});
+  // await page.locator('#aviso .close').click();
+  // await page.selectOption('#sedes_id', {label: 'Parque Bosques del Norte'});
+  // await page.selectOption('#deportes_id', {label: 'Tenis'});
   
-  await page.click('.btn[value="Buscar"]');
+  // await page.locator('.btn[value="Buscar"]').click();
   
-  const date = new Date();
-  date.setDate(date.getDate() + 1);
-  const tomorrowStr = date.toISOString().split('T')[0];
+  // const date = new Date();
+  // date.setDate(date.getDate() + 1);
+  // const tomorrowStr = date.toISOString().split('T')[0];
   
-  const button = `button[data-date="${tomorrowStr}"][data-time="16"]`;
-  await page.locator(button).click();
+  // const button = `button[data-date="${tomorrowStr}"][data-time="16"]`;
+  // await page.locator(button).click();
   
-  await page.locator('#nombre').fill('Juan Rois');
-  await page.locator('#cedula').fill('1234567890');
-  await page.locator('#telefono').fill('3010000000');
-  await page.locator('#correo').fill('juan.rois@email.com');
-  await page.locator('#canchas_id').selectOption({label: 'Cancha 3'});
-  await page.locator('#aceptaTerminos').check();
-  await page.locator('button[type="submit"]').click();
+  // await page.locator('#nombre').fill('Bryan Algutria');
+  // await page.locator('#cedula').fill('1045714378');
+  // await page.locator('#telefono').fill('3017964585');
+  // await page.locator('#correo').fill('brayan.arodriguez@gmail.com');
+  // await page.locator('#canchas_id').selectOption({label: 'Cancha 3'});
+  // await page.locator('#aceptaTerminos').check();
+  // await page.locator('button[type="submit"]').click();
 
-  notifier.notify({
-    title: 'Cancha Reservada!!',
-    message: `Se reservó la cancha para el: ${tomorrowStr} a las 8PM`
-  })
+  // notifier.notify({
+  //   title: 'Cancha Reservada!!',
+  //   message: `Se reservó la cancha para el: ${tomorrowStr} a las 8PM`
+  // })
   
-  await browser.close()
+  // await browser.close()
 })
